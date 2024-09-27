f2ca3e71af8e1c6fe6a2d0dab914dbc0470c50af
Amir Rahatloo <Rahatloom@gmail.com>
2024-09-27 02:31:01 -0700

    Update XGBoost version and build process

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -o errexit
 
 pip install --upgrade pip
 pip install --no-cache-dir numpy scipy pandas scikit-learn
-pip install --no-cache-dir -r requirements.txtv
+pip install --no-cache-dir -r requirements.txt
